e95d7fbc6c282b14be1a5a0157242febf95b6ece
Kent Gruber <kgruber1@emich.edu>
2016-12-03 13:11:56 -0500

    Add Vagrant file

--- controllers/index.rb
+++ controllers/index.rb
@@ -4,9 +4,11 @@ module Sinatra
       module Index
         def self.registered(app)
 
+          # This specifies the main route of out application.
           app.get '/' do
             erb :index
           end
+
         end
       end
     end
